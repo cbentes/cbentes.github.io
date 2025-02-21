8488b9e23122bc3c59b5882f0c7bd82111aaf771
Carlos Bentes <carlos@cbentes.com>
2025-02-21 13:14:20 +0200

    update GA

---
+++ _static/gtag_activate.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SJ3VEG6NXY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-SJ3VEG6NXY');
+</script>
